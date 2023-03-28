25753f98af4102f8aa653bfefae6421db8d01794
Vaibhav Dileep Pillai <111226733+vibes-863@users.noreply.github.com>
2023-03-28 22:21:35 +0800

    moveToGoal works on the turtlebot.
    The code which makes the turtlebot go to the table which is specified through the keyboard. The points are mentioned in the array.

--- moveToGoal.py
+++ moveToGoal.py
@@ -8,8 +8,11 @@ import numpy as np
 import json ## I added this to retrieve the coordinates from a file
 
 # constants
-rotatechange = 0.1
+rotatechange = 0.5
 speedchange = 0.05
+angle_error = 5 # in degrees
+x_error = 0.05 
+y_error = 0.05 
 
 # defining the individual tables 'points' based on the wayPointsData.json file
 table1 = [1,2] 
@@ -18,6 +21,7 @@ table3 = [1,4]
 table4 = [1,5]
 table5 = [1,6,7]
 table6 = [1,8,9]
+
 
 # code from https://automaticaddison.com/how-to-convert-a-quaternion-into-euler-angles-in-python/
 def euler_from_quaternion(x, y, z, w):
@@ -48,20 +52,21 @@ class Navigate(Node):
     def __init__(self):
         super().__init__('moveToGoal')
         self.publisher_ = self.create_publisher(Twist,'cmd_vel',10)
-        # self.get_logger().info('Created publisher')
 
-        ## create subscription to get location values from map2base
+        # create subscription to get location values from map2base
         self.map2base_subscription = self.create_subscription(
             Pose,
             '/map2base',
             self.map2base_callback,
             10)
         self.map2base_subscription # prevent unused variable warning
+        # initialize variables
         self.x_coordinate = 0 # x-coordinate of the bot in the map
         self.y_coodinate = 0 # y-coordinate of the bot in the map
         self.roll = 0 # roll of the bot in the map (not needed)
         self.pitch = 0 # pitch of the bot in the map (not needed)
         self.yaw = 0 # yaw of the bot in the map (needed)
+
 
     # function to set the class variables using the map2base information
     def map2base_callback(self, msg):
@@ -72,6 +77,7 @@ class Navigate(Node):
         position_map = msg.position
         self.x_coordinate = position_map.x
         self.y_coodinate = position_map.y
+
 
     # function to rotate the TurtleBot
     def rotatebot(self, rot_angle):
@@ -126,61 +132,88 @@ class Navigate(Node):
         # stop the rotation
         self.publisher_.publish(twist)
 
+
     # function to move to goal
     def moveToGoal(self, next_x, next_y):
+        # create Point object
         goal = Point()
+        # create Twist object
+        twist = Twist()
+
+        # the x-coordinate we need to go to
         goal.x = next_x
+        # the y-coordinate we need to go to
         goal.y = next_y
+        # define reached variable to end the while loop when the robot reaches a point
         reached = False
 
-        speed = Twist()
-
+        # allow the callback functions to run
         rclpy.spin_once(self)
+        # inc_x is the difference in the x-coordinate between goal and current
         inc_x = goal.x - self.x_coordinate
+        # inc_y is the difference in y-coordinate between goal and current
         inc_y = goal.y - self.y_coodinate
-        
-        self.get_logger().info('inc_x: %f inc_y: %f' %(inc_x, inc_y))
+        # self.get_logger().info('inc_x: %f inc_y: %f' %(inc_x, inc_y))
 
+        # angle_to_coordinate uses the atan2 function to compute the angle from x-axis to the coordinate in the counter-clockwise direction
         angle_to_goal = math.degrees(math.atan2(inc_y, inc_x))
-        #self.get_logger().info(('angle is: %f' %(angle_to_goal)))
-            
-        if abs(angle_to_goal - math.degrees(self.yaw)) > 5:
-                #self.get_logger().info('in angle thing')
-                self.get_logger().info('angleToGoal: %f' % (angle_to_goal - math.degrees(self.yaw)))
-                self.rotatebot(angle_to_goal - math.degrees(self.yaw))
-        self.get_logger().info('moving straight to goal')
-        while (reached == False):
-            rclpy.spin_once(self)
+        # angle_to_turn stores the angle between the robots 0 degree and the coordinate
+        angle_to_turn = angle_to_goal - math.degrees(self.yaw)
+        # the if statement checks if the angle to turn is > angle_error   
+        if abs(angle_to_turn) > angle_error:
+                self.get_logger().info('Turning: %f degrees' % (angle_to_turn))
+                # calls the rotatebot function to rotate the robot angle_to_turn degrees
+                self.rotatebot(angle_to_turn)
+        # prints that the robot is facing the goal
+        self.get_logger().info('Facing goal. Now going straight.')
 
-            if ((abs(goal.x - self.x_coordinate) > 0.05) or (abs(goal.y - self.y_coodinate) > 0.05)):        
-                speed.linear.x = 0.5
-                speed.angular.z = 0.0
+        # the while loop continues until the robot reaches the goal
+        while (reached == False):
+            # allow the callback functions to run
+            rclpy.spin_once(self)
+            # the if statement checks if the difference between the goal's, x or y, coordinate and the current, x or y, coordinate is > x_error or > y_error respecitively
+            if ((abs(goal.x - self.x_coordinate) > x_error) or (abs(goal.y - self.y_coodinate) > y_error)):
+                # move forward
+                twist.linear.x += speedchange
+                twist.angular.z = 0.0
+            
             else:
-                speed.linear.x = 0.0
-                speed.angular.z = 0.0
-                reached = True       
-            self.publisher_.publish(speed)
-        #self.get_logger().info('reached goal')
+                # stop moving
+                twist.linear.x = 0.0
+                twist.angular.z = 0.0
+                # changed the reached variable to True in order to exit the loop
+                reached = True
+
+            # publish the movement commands
+            self.publisher_.publish(twist)
+
+        # self.get_logger().info('reached goal')
+
 
     # function to use waypoints to navigate to individual Tables
     def moveToTable(self, table_num):
         # loads the coordinate data from the wayPointsData.json file into the variable data, as a dictionary
         with open('/home/vaibhav/colcon_ws/src/auto_nav/auto_nav/wayPointsData.json') as f:
             data = json.load(f) 
-
         # returns the array of the table mentioned by table_num
         current_table = globals()[f"table{table_num}"]
-        #current_table = table1
 
+        # goes through each point in the array of current_table
         for point_number in current_table:
+            # extracts the x-coordinate of the point in the current_table's array
             x_cord = data['point' + str(point_number)]['x_cord']
+            # extracts the y-coordinate of the point in the current_table's array
             y_cord = data['point' + str(point_number)]['y_cord']
+            # extracts the orientation of the robot at the point in the currect_table's array
             #orientation = data['point' + point_number]['orientation']  Not using as of now
 
+            # calls the function to move the robot to the point
             self.moveToGoal(x_cord,y_cord)
+            # prints to the terminal that the point has been reached
             self.get_logger().info('Reached point %d' %(point_number))
-        
+        #prints to the terminal that the table has been reached
         self.get_logger().info('Reached table %d' %(table_num))
+
 
     # temporary function to type in the table number
     def readKey(self):
@@ -189,9 +222,19 @@ class Navigate(Node):
                 # get keyboard input
                 cmd_char = int(input("Please enter the table number: "))
                 
+                # move to table number cmd_char
                 self.moveToTable(cmd_char)
+
         except Exception as e:
             print(e)
+        
+        # Ctrl-c detected
+        finally:
+            twist = Twist()
+        	# stop moving
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            self.publisher_.publish(twist)
 
 
 def main(args=None):
